0518809979e785770c0a28e9cf3d21d9677f47bf
balcarcelmar <maryangela140505@gmail.com>
2025-01-27 20:39:55 -0500

    anim y texto listo

--- reto/anim/script.js
+++ reto/anim/script.js
@@ -6,10 +6,10 @@ backgroundMusic.stop();
 // Contador de caracteres
 const textInput = document.getElementById("text-input");
 const charCount = document.getElementById("char-count");
-
 textInput.addEventListener("input", () => {
   charCount.textContent = textInput.value.length;
 });
+
 
 const bici = document.getElementById("bici");
 
